007e7f70e2bd649da687ed5cf679dda8d85b6397
KariNoir <KariNoirm@gmail.com>
2020-02-02 16:08:05 -0500

    add new test class for cat

--- src/AmokVirtualPet.java
+++ src/AmokVirtualPet.java
@@ -1,4 +1,76 @@
 
 public class AmokVirtualPet {
 
+	private int hunger = 6; // child 
+	private int thirst = 6;
+	private int boredom = 6;
+	private int walk = 6;
+	private String petName;
+	private String petDescription;
+//	 
+	public AmokVirtualPet(String petNameParameter, String petDescriptionParameter) {
+		this.petName = petNameParameter;
+		this.petDescription = petDescriptionParameter;
+	}
+	public AmokVirtualPet() {
+		// default constructor - was invisible 
+	}
+	public AmokVirtualPet(String petNameParameter, String petDescriptionParameter, int hungerParameter, 
+			int thirstParameter, int boredomParameter, int walkParameter) {
+		this.petName = petNameParameter;
+		this.petDescription = petDescriptionParameter;
+		this.hunger = hungerParameter;
+		this.thirst = thirstParameter;
+		this.boredom = boredomParameter;
+		this.walk = walkParameter;
+	}
+	public AmokVirtualPet(int i, int j, int k, int l) {
+		// TODO Auto-generated constructor stub
+		//add to your's
+	}
+	public int getWalk() {
+		return this.walk;
+	}
+	public int getHunger() { //parent- protects the data by wrapping it in a method
+		return this.hunger;
+	}
+	public int getThirst() {
+		return this.thirst;
+	}
+	public int getBoredom() {
+		return this.boredom;
+	}
+	public void eat() {
+		this.hunger=-3; //this cookie!
+		//same as this.hunger = this.hunger -1;
+	}
+	public void drink() {
+		this.thirst=-3;
+	}
+	public void play() {
+		this.boredom=-3;
+	}
+	public void tick() {
+		hunger--;
+		thirst--;
+		boredom--;
+	}
+	public String getPetName() {
+		return this.petName;
+	}
+	public String getPetDescription() {
+		return this.petDescription;
+	}
+	public void add(Object pet1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void put(String petName2, AmokVirtualPet pet) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void withdraw(int position) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/AmokVirtualPetShelterAppTest.java
+++ src/AmokVirtualPetShelterAppTest.java
@@ -4,9 +4,151 @@ import org.junit.jupiter.api.Test;
 
 class AmokVirtualPetShelterAppTest {
 
+//	OLD STUFF
+//	Create a main method that…
+//	implements a game loop.
+//	asks for user input.
+//	writes output to the console.
+//	calls VirtualPetShelter’s tick method after each interaction
+//	Available user interface actions should include (at minimum)…
+//	feeding all the pets
+//	watering all the pets
+//	playing with an individual pet, which should display a list of pet names and descriptions, allowing a user to select one
+//	allow adoption of a pet, which should display a list of pet names and descriptions, allowing a user to select one
+//	allow intake of a pet, prompting the user for the pet’s information, requiring the user to (at minimum) specify name and description
+//	
+//	include appropriate instance variable(s) to store the pets who reside at the shelter
+//	include methods that:
+//	return a Collection of all of the pets in the shelter
+//	return a specific VirtualPet given its name
+//	allow intake of a homeless pet
+//	allow adoption of a homeless pet
+//	feed all of the pets in the shelter
+//	water all of the pets in the shelter
+//	plays (or performs some other interaction(s)) with an individual pet in the shelter
+//	
+//	
+// NEW STUFF
+//	walk all dogs
+//	clean dog cages
+//	clean the shelter litterbox
+//	oil all robotic pet
+	
+	AmokVirtualPet underTest = new AmokVirtualPet(); // calling default constructor
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void shouldWalkDog() {
+		// act
+		int result = underTest.getWalk(); // makes a method in VP2 class
+		// assert
+		assertEquals(6, result);
 	}
-
 }
+	
+//	@Test
+//	public void hungerShouldDecreaseBy1AfterEat() {
+//		// act
+//		int beforeEat = underTest.getHunger();
+//		underTest.eat(); // void - just eats
+//		int result = underTest.getHunger();
+//		// assert
+//		assertEquals(beforeEat - 1, result);
+//	}
+//	@Test
+//	public void thirstShouldBe0AtStart() {
+//		// act
+//		int result = underTest.getThirst(); // makes a method in VP2 class
+//		// assert
+//		assertEquals(0, result);
+//	}
+//	@Test
+//	public void thirstShouldDecreaseBy1AfterDrink() {
+//		// act
+//		int beforeDrink = underTest.getThirst();
+//		underTest.drink(); // void - just eats
+//		int result = underTest.getThirst();
+//		// assert
+//		assertEquals(beforeDrink - 1, result);
+//	}
+//	@Test
+//	public void boredomShouldBe0AtStart() {
+//		// act
+//		int result = underTest.getBoredom(); // makes a method in VP2 class
+//		// assert
+//		assertEquals(0, result);
+//	}
+//	@Test
+//	public void boredomShouldDecreaseBy1AfterPlay() {
+//		// act
+//		int beforePlay = underTest.getBoredom();
+//		underTest.play(); // void - just eats
+//		int result = underTest.getBoredom();
+//		// assert
+//		assertEquals(beforePlay - 1, result);
+//	}
+//	@Test
+//	public void boredomShouldIncreaseBy1AfterTick() {
+//		// Act
+//		int beforePlay = underTest.getBoredom();
+//		underTest.tick();
+//		int result = underTest.getBoredom();
+//		assertEquals(beforePlay + 1, result);
+//	}
+//	@Test
+//	public void thirstShouldIncreaseBy1AfterTick() {
+//		// Act
+//		int beforeDrink = underTest.getThirst();
+//		underTest.tick();
+//		int result = underTest.getThirst();
+//		assertEquals(beforeDrink + 1, result);
+//	}
+//	@Test
+//	public void hungerShouldIncreaseBy1AfterTick() {
+//		// Act
+//		int beforeEat = underTest.getHunger();
+//		underTest.tick();
+//		int result = underTest.getHunger();
+//		// assert
+//		assertEquals(beforeEat + 1, result);
+//	}
+//	@Test
+//	public void shouldReturnBobAndLittleAndCuteOnInitialization() {
+//		//arrange
+//		VirtualPet2 petBio = new VirtualPet2("Bob", "little and cute!");
+//		//act
+//		String petName = petBio.getPetName();
+//		String petDescription = petBio.getPetDescription();
+//		//assert
+//		assertEquals("Bob", petName);
+//		assertEquals("little and cute!", petDescription);
+//	}
+//	@Test
+//	public void shouldReturnFluffyAndTiredOnInitialization() {
+//		//arrange
+//		VirtualPet2 petBio = new VirtualPet2("Fluffy", "Tired");
+//		//act
+//		String petName = petBio.getPetName();
+//		String petDescription = petBio.getPetDescription();
+//		//assert
+//		assertEquals("Fluffy", petName);
+//		assertEquals("Tired", petDescription);
+//	}
+//	@Test
+//	public void shouldReturnDefaultValues() {
+//		//return name, description, hunger, thirst, and boredom
+//		//arrange
+//		VirtualPet2 fullPet = new VirtualPet2("Fluffy", "Tired", 2, 3, 4);
+//		//act
+//		String petName = fullPet.getPetName();
+//		String petDescription = fullPet.getPetDescription();
+//		int petHunger = fullPet.getHunger();
+//		int petThirst = fullPet.getThirst();
+//		int petBoredom = fullPet.getBoredom();
+//		//assert
+//		assertEquals("Fluffy", petName);
+//		assertEquals("Tired", petDescription);
+//		assertEquals(2, petHunger);
+//		assertEquals(3, petThirst);
+//		assertEquals(4, petBoredom);
+//	}
+//}
+//}
